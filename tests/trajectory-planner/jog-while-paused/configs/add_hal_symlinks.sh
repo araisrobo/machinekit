81cd7ff4e709eeff0bda5367e8c879e99c6a93a6
Yishin Li <ysli@araisrobo.com>
2014-12-28 21:07:54 +0800

    WIP: add testing env, jog-while-paused/, for jog-fwd and jog-rev

---
+++ tests/trajectory-planner/jog-while-paused/configs/add_hal_symlinks.sh
@@ -0,0 +1,4 @@
+ln -sf ../../../../configs/sim/core_sim.hal
+ln -sf ../../../../configs/sim/axis_manualtoolchange.hal
+ln -sf ../../../../configs/sim/sim_spindle_encoder.hal
+ln -sf ../../../../configs/sim/simulated_home.hal
