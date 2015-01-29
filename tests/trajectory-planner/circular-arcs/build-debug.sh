f5f4de444029b618c6e1c3c743140ead9038f879
Yishin Li <ysli@araisrobo.com>
2015-01-29 16:08:24 +0800

    Update EXTRA_DEBUG to Makefile for TP_DEBUG at build-debug.sh

--- tests/trajectory-planner/circular-arcs/build-debug.sh
+++ tests/trajectory-planner/circular-arcs/build-debug.sh
@@ -3,7 +3,10 @@ cd ../../../src
 #Ugly way to force rebuild of kinematics, which assumes that tp_debug isn't
 #used anywhere else...
 touch emc/tp/t[cp]*.[ch]
+CONCURRENCY_LEVEL=`getconf _NPROCESSORS_ONLN`
+#make EXTRA_DEBUG='-DTP_DEBUG'
 #make EXTRA_DEBUG='-DTC_DEBUG -DTP_DEBUG'
 #make EXTRA_DEBUG='-DTP_DEBUG -DTP_INFO_LOGGING'
-make EXTRA_DEBUG='-DTC_DEBUG -DTP_DEBUG -DTP_INFO_LOGGING'
-#make EXTRA_DEBUG='-DTP_DEBUG'
+#make EXTRA_DEBUG='-DTC_DEBUG -DTP_DEBUG -DTP_INFO_LOGGING'
+make -j${CONCURRENCY_LEVEL} OPT='-O0' EXTRA_DEBUG='-DTC_DEBUG -DTP_DEBUG -DTP_INFO_LOGGING' V=1 && sudo make setuid
+cd -
