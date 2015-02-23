eecd81dabb9c0670eddfa7c7f5930a885a790194
Yishin Li <ysli@araisrobo.com>
2015-02-23 14:34:19 +0800

    Prepare testing scripts for "G64 P<tolerance>" motion
    * axis_jerk_mm.ini: config as no-homing
    * tests/trajecotry-planner/circular-arcs/
      + nc_files/quick-tests/square_g64_tol.ngc ... G64 P<tolerance>
      + analysis.gnu ... gnuplot script for analyzing s-curve velocity
                         profile of configs/tp.log
      + test-jerk.sh ... load square_g64_tol.ngc for testing

--- tests/trajectory-planner/circular-arcs/test-jerk.sh
+++ tests/trajectory-planner/circular-arcs/test-jerk.sh
@@ -2,7 +2,8 @@ sudo cp /dev/null /var/log/linuxcnc.log
 cp position.blank position.txt
 mv constraints.log constraints_old.log
 linuxcnc configs/axis_jerk_mm.ini | tee test.log &
-python machine_setup.py ../nc_files/quick-tests/square.ngc
+# python machine_setup.py ../nc_files/quick-tests/square.ngc
+python machine_setup.py ../nc_files/quick-tests/square_g64_tol.ngc
 fg
 ./process_runlog.sh test.log movement.txt
 if [ -a movement.txt ] 
