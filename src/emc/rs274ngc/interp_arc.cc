9208d393ee39ce828a635ea35a5807a772f96e6e
Jofey Jian <jofey@araisrobo.com>
2015-04-10 18:46:40 +0800

    without check radius for G2/G3

--- src/emc/rs274ngc/interp_arc.cc
+++ src/emc/rs274ngc/interp_arc.cc
@@ -111,8 +111,8 @@ int Interp::arc_data_comp_ijk(int move,  //!<either G_2 (cw arc) or G_3 (ccw arc
   CHKS(abs_err > 100*tolerance || (abs_err > tolerance && (rel_err > .001)),
       _("Radius to end of arc differs from radius to start: "
        "start=(%c%.4f,%c%.4f) center=(%c%.4f,%c%.4f) end=(%c%.4f,%c%.4f) r1=%.4f r2=%.4f abs_err=%.4g rel_err=%.4f%%"),
-       a, current_x, b, current_y, 
-       a, *center_x, b, *center_y, 
+       a, current_x, b, current_y,
+       a, *center_x, b, *center_y,
        a, end_x, b, end_y, arc_radius, radius2,
        abs_err, rel_err*100);
 
@@ -275,13 +275,13 @@ int Interp::arc_data_ijk(int move,       //!< either G_2 (cw arc) or G_3 (ccw ar
   CHKS(((radius < min_radius) || (radius2 < min_radius)), NCE_ZERO_RADIUS_ARC);
   double abs_err = fabs(radius - radius2);
   double rel_err = abs_err / std::max(radius, radius2);
-  CHKS(abs_err > 100*tolerance || (abs_err > tolerance && (rel_err > .001)),
-      _("Radius to end of arc differs from radius to start: "
-       "start=(%c%.4f,%c%.4f) center=(%c%.4f,%c%.4f) end=(%c%.4f,%c%.4f) r1=%.4f r2=%.4f abs_err=%.4g rel_err=%.4f%%"),
-       a, current_x, b, current_y, 
-       a, *center_x, b, *center_y, 
-       a, end_x, b, end_y, radius, radius2,
-       abs_err, rel_err*100);
+//  CHKS(abs_err > 100*tolerance || (abs_err > tolerance && (rel_err > .001)),
+//      _("Radius to end of arc differs from radius to start: "
+//       "start=(%c%.4f,%c%.4f) center=(%c%.4f,%c%.4f) end=(%c%.4f,%c%.4f) r1=%.4f r2=%.4f abs_err=%.4g rel_err=%.4f%% tolerance=%.4f%%"),
+//       a, current_x, b, current_y,
+//       a, *center_x, b, *center_y,
+//       a, end_x, b, end_y, radius, radius2,
+//       abs_err, rel_err*100, tolerance);
   if (move == G_2)
     *turn = -1 * p_number;
   else if (move == G_3)
