4bbf7421b19c2e10cb5e636a101b9209430ba4eb
Yishin Li <ysli@araisrobo.com>
2015-12-23 18:13:15 +0800

    clean-up sim/ini files

--- configs/sim/axis/mkwrapper_mm.py
+++ configs/sim/axis/mkwrapper_mm.py
@@ -3,24 +3,27 @@
 import sys
 import os
 import subprocess
-import importlib
+import time
 from machinekit import launcher
-from time import *
 
 launcher.register_exit_handler()
-launcher.set_debug_level(5)
+launcher.set_debug_level(3)
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 try:
-    launcher.check_installation()                                     # make sure the Machinekit installation is sane
-    launcher.cleanup_session()                                        # cleanup a previous session
-    # launcher.load_bbio_file('myoverlay.bbio')                       # load a BBB universal overlay
-    launcher.start_process("configserver ~/proj/remote-ui/Machineface")   # start the configserver
-    launcher.start_process('linuxcnc mkwrapper_mm.ini')               # start linuxcnc
+    launcher.check_installation()
+    launcher.cleanup_session()
+
+    # the point-of-contact for QtQUickVCP
+    launcher.start_process("configserver -d -n MKSim ~/proj/remote-ui/Machineface")
+    launcher.start_process('linuxcnc mkwrapper_mm.ini')
+
+    while True:
+        launcher.check_processes()
+        time.sleep(1)
+
 except subprocess.CalledProcessError:
     launcher.end_session()
     sys.exit(1)
 
-while True:
-    sleep(1)
-    launcher.check_processes()
+sys.exit(0)
