93cbd4e932b28a446d6b87e80782f8bb09bd88e1
Yishin Li <ysli@araisrobo.com>
2016-01-04 17:29:32 +0800

    update configs/sim/axis/go.sh

--- configs/sim/axis/go.sh
+++ configs/sim/axis/go.sh
@@ -1,3 +1,8 @@
-linuxcnc_stop.sh
+linuxcnc_stop
 sudo cp /dev/null /var/log/linuxcnc.log
+
 DEBUG=5 linuxcnc axis_jerk_mm.ini | tee run.log
+
+# for remote-ui
+# mklauncher . &
+# python mkwrapper_mm.py -c -i mkwrapper_mm.ini
