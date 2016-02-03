fee4500fa50ab5535cfed9a5f56d2ff4edd578dd
Yishin Li <ysli@araisrobo.com>
2016-02-03 20:21:40 +0800

    Update configs for RA605 robot

--- configs/by_interface/arais/puma/halcmd.sh
+++ configs/by_interface/arais/puma/halcmd.sh
@@ -4,15 +4,15 @@
 
 echo "bp-tick: " `halcmd getp wosi.bp-tick`
 
-# for a in 0 1 2 3 4 5; do
-for a in 4; do
-echo "enc_pos[$a]:   " `halcmd getp wosi.stepgen.$a.enc_pos`
-echo "cmd_pos[$a]:   " `halcmd getp wosi.stepgen.$a.cmd-pos`
-echo "rawcount32[$a]:   " `halcmd getp wosi.stepgen.$a.rawcount32`
+for a in 0 1 2 3 4 5; do
+# for a in 4; do
+# echo "enc_pos[$a]:   " `halcmd getp wosi.stepgen.$a.enc_pos`
+# echo "cmd_pos[$a]:   " `halcmd getp wosi.stepgen.$a.cmd-pos`
+# echo "rawcount32[$a]:   " `halcmd getp wosi.stepgen.$a.rawcount32`
 # echo "ferror[$a]:    " `halcmd getp wosi.stepgen.$a.ferror-flag`
-# echo "axis.$a.joint-pos-cmd:   " `halcmd getp axis.$a.joint-pos-cmd`
+echo "axis.$a.joint-pos-cmd:   " `halcmd getp axis.$a.joint-pos-cmd`
 # echo "axis.$a.joint-pos-fb:    " `halcmd getp axis.$a.joint-pos-fb`
-# echo "axis.$a.motor-pos-cmd:   " `halcmd getp axis.$a.motor-pos-cmd`
+echo "axis.$a.motor-pos-cmd:   " `halcmd getp axis.$a.motor-pos-cmd`
 # echo "axis.$a.motor-pos-fb:    " `halcmd getp axis.$a.motor-pos-fb`
 # # echo "axis.$a.probed-pos:      " `halcmd getp axis.$a.probed-pos`
 # echo "wosi.stepgen.$a.position-cmd:   " `halcmd getp wosi.stepgen.$a.position-cmd`
@@ -27,11 +27,11 @@ done
 # # echo "debug-$a:      " `halcmd getp wosi.debug.value-$a`
 # # done
 # 
-echo "debug-00,  pid[4].error     ......" `halcmd getp wosi.debug.value-00`
-echo "debug-01,  pid[4].cmd_d     ......" `halcmd getp wosi.debug.value-01`
-echo "debug-02,                   ......" `halcmd getp wosi.debug.value-02`
-echo "debug-03,  joint_vel_cmd[4] pid..." `halcmd getp wosi.debug.value-03`
-echo "debug-04,  joint_vel_cmd[4] pwm..." `halcmd getp wosi.debug.value-04`
-echo "debug-05,  joint_cmd[4]     ......" `halcmd getp wosi.debug.value-05`
-echo "debug-06,  pwm cmd          ......" `halcmd getp wosi.debug.value-06`
-echo "debug-07,  machine-on       ......" `halcmd getp wosi.debug.value-07`
+# echo "debug-00,  pid[4].error     ......" `halcmd getp wosi.debug.value-00`
+# echo "debug-01,  pid[4].cmd_d     ......" `halcmd getp wosi.debug.value-01`
+# echo "debug-02,                   ......" `halcmd getp wosi.debug.value-02`
+# echo "debug-03,  joint_vel_cmd[4] pid..." `halcmd getp wosi.debug.value-03`
+# echo "debug-04,  joint_vel_cmd[4] pwm..." `halcmd getp wosi.debug.value-04`
+# echo "debug-05,  joint_cmd[4]     ......" `halcmd getp wosi.debug.value-05`
+# echo "debug-06,  pwm cmd          ......" `halcmd getp wosi.debug.value-06`
+# echo "debug-07,  machine-on       ......" `halcmd getp wosi.debug.value-07`
