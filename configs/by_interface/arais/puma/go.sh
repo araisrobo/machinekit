a3e342990cc9ced3525a44676d871f4923f4ba37
Yishin Li <ysli@araisrobo.com>
2016-02-04 19:52:11 +0800

    Add RA605-TRIV.ini for trivkins

--- configs/by_interface/arais/puma/go.sh
+++ configs/by_interface/arais/puma/go.sh
@@ -8,6 +8,7 @@ mklauncher . &
 # -f: --fpga, load fpga bit file
 # -s: --startpy, start python scripts
 # python run.py -f -c -s -d -iVBC3.ini
-python run.py -f -c -s -iRA605.ini
+# python run.py -f -c -s -iRA605.ini
+python run.py -f -c -s -iRA605-TRIV.ini
 # python run.py -f -c -s -iVBC3_noH.ini
 # python run.py -c -d -iVBC3-SIM.ini
