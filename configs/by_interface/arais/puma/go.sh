af526a290575c1ef157bfed5236acb9e9fafa085
Yishin Li <ysli@araisrobo.com>
2016-03-05 15:59:08 +0800

    Update configs and nc_files for RA605

--- configs/by_interface/arais/puma/go.sh
+++ configs/by_interface/arais/puma/go.sh
@@ -9,7 +9,7 @@ mklauncher . &
 # -s: --startpy, start python scripts
 # python run.py -f -c -s -d -iVBC3.ini
 # python run.py -f -c -s -iRA605.ini
-# python run.py -f -c -s -iRA605-TRIV.ini
-python run.py -f -c -iRA605-TRIV-noH.ini    # no "-s", for updating abs-enc-pos
+python run.py -f -c -s -iRA605-TRIV.ini
+# python run.py -f -c -iRA605-TRIV-noH.ini    # no "-s", for updating abs-enc-pos
 # python run.py -f -c -s -iVBC3_noH.ini
 # python run.py -c -d -iVBC3-SIM.ini
